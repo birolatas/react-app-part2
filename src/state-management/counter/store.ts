4fabe027e5f296ef0634e4e60cfc51a70a9ae6ec
Birol Ataş <birolatas@hotmail.com>
2024-02-12 17:43:30 +0300

    inspecting stores with Zustand DevTools

--- src/state-management/counter/store.ts
+++ src/state-management/counter/store.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { mountStoreDevtool } from "simple-zustand-devtools";
 
 interface CounterStore {
   counter: number;
@@ -13,5 +14,11 @@ const useCounterStore = create<CounterStore>((set) => ({
   increment: () => set((store) => ({ counter: store.counter + 1 })),
   reset: () => set(() => ({ max: 10 })),
 }));
+
+// In the Components tab of the browser you can see the "devtool Counter Store" row on the top.
+// If you select this row you can see the content of the store.
+// You can change any of this values.
+if (process.env.NODE_ENV === "development")
+  mountStoreDevtool("Counter Store", useCounterStore);
 
 export default useCounterStore;
